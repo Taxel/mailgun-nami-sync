031b377abc48ed2442786f0f8077e123a2dd5c8f
Andreas Engl <ae@cephea.de>
2016-12-28 22:01:33 +0100

    Moved site CSS to App component

--- app/App.tsx
+++ app/App.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 
+let styles = require('./App.css');
+
 export class App extends React.Component<{}, {}> {
   render() {
     return <h1>Hello!</h1>;
--- app/index.tsx
+++ app/index.tsx
@@ -2,6 +2,4 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { App } from './App';
 
-require('./index.css');
-
 ReactDOM.render(<App />, document.getElementById('root'));
