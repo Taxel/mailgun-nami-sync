5945032b1c9138fa850e291e4506e850339d7a84
Andreas Engl <ae@cephea.de>
2016-12-28 20:43:28 +0100

    Improved boilerplate

--- app/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { App } from './App';
-
-require('./index.css');
-
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
