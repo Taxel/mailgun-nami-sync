02a2a4124c6f749674cb5640bdbf4ef458cbd97a
Andreas Engl <ae@cephea.de>
2016-12-31 23:01:06 +0100

    Optimised production version

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 const {app, BrowserWindow} = require('electron');
 const path = require('path');
 const url = require('url');
+const webpack = require('webpack');
+const config = require('./webpack.config.js');
+
+config.plugins.push(
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  })
+);
 
 function createWindow() {
-  let window = new BrowserWindow({ width: 800, height: 600 });
+  let window = new BrowserWindow({ width: 600, height: 800 });
   window.loadURL(url.format({
     pathname: path.join(__dirname, './build/index.html'),
     protocol: 'file:',
