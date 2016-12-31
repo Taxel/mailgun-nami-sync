7498587f2468fc6954332e528ecb78c3601cfa15
Andreas Engl <ae@cephea.de>
2016-12-31 22:29:08 +0100

    Save & load state using localStorage

--- app/App.tsx
+++ app/App.tsx
@@ -7,10 +7,35 @@ require('./App.css');
 require('./medium-editor.css');
 require('./medium-beagle.css');
 
-export class App extends React.Component<{}, {}> {
+interface Props {}
+
+interface State {
+  key?: string;
+  [name: string]: any;
+}
+
+export class App extends React.Component<Props, State> {
+  saveTimer: number;
+  refs: {
+    key: HTMLInputElement;
+    [key: string]: any;
+  }
+
   constructor() {
     super();
     this.onSubmit = this.onSubmit.bind(this);
+    this.syncState = this.syncState.bind(this);
+    this.saveState = this.saveState.bind(this);
+    this.state = {
+      key: ''
+    };
+    const settings = window.localStorage.getItem('settings');
+    if (settings) {
+      const settingsDecoded = JSON.parse(window.localStorage['settings'] );
+      for (let key in settingsDecoded) {
+        this.state[key] = settingsDecoded[key];
+      }
+    }
   }
 
   componentDidMount() {
@@ -34,9 +59,12 @@ export class App extends React.Component<{}, {}> {
         </div>
         <div className='form-group'>
           <input
+            ref='key'
             type='key'
             className='form-control'
-            placeholder='API key' />
+            placeholder='API key'
+            value={this.state.key}
+            onChange={this.syncState}/>
         </div>
         <div className='form-group'>
           <div className='App-editor form-control' />
@@ -49,15 +77,36 @@ export class App extends React.Component<{}, {}> {
     );
   }
 
+  syncState() {
+    this.setState({
+      key: this.refs.key.value
+    });
+    // Trigger a save
+    if (this.saveTimer) {
+      window.clearTimeout(this.saveTimer);
+    }
+    this.saveTimer = window.setTimeout(this.saveState, 1000);
+  }
+
+  saveState() {
+    window.localStorage['settings'] = JSON.stringify(this.state);
+    console.log('Saved settings', window.localStorage);
+  }
+
   onSubmit(event: React.MouseEvent<HTMLButtonElement>) {
     event.preventDefault();
     event.stopPropagation();
-    request({
-      method: 'POST',
+    request.post({
       url: 'https://api.mailgun.net/v3/sector4.life/messages',
       auth: {
         user: 'api',
-        pass: 'key-foo'
+        pass: this.state.key
+      },
+      form: {
+        from: 'no-reply@sector4.life',
+        to: 'launch@sector4.life',
+        subject: 'Newsletter',
+        text: 'Test!'
       }
     }, this.onRequestResponse);
   }
