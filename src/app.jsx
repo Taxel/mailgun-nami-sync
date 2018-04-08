aef31ebfc95b03cace61c163be91ea5354646f9c
Alexander Theimer <x.taxel.x@gmail.com>
2018-04-08 04:11:54 +0200

    added nami search, material UI

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
-import http from 'http';
+import secret_keys from './lib/keys.json';
+import NamiAPI from './lib/nami.js';
+
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+import {List, ListItem, DropDownMenu, MenuItem, Toggle, Paper} from 'material-ui';
 
 export default class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      namiInitialized: false,
+      namiData: null,
+      stufe: 1,
+      leiter: true
+    }
+    this.nami = new NamiAPI(secret_keys.nami_user, secret_keys.nami_pw);
+    this.nami.startSession().then((success)=>{this.setState({namiInitialized: true}); this.namiSearch(this.state.stufe, this.state.leiter)}, (error)=>{console.error(error)});
+    this.namiSearch = this.namiSearch.bind(this);
+    this.changeStufe = this.changeStufe.bind(this);
+  }
   sendMail(){
-    var api_key = 'key-1f632b8782f4a818f19c91a4cffa6dfd';
-    var domain = 'sandboxc2ea7d6c06b340e7a5336b0576e55bed.mailgun.org';
-    var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
+    var mailgun = require('mailgun-js')({apiKey: secret_keys.key, domain: secret_keys.domain});
 
     var data = {
       "from": "Mailgun Sandbox <postmaster@sandboxc2ea7d6c06b340e7a5336b0576e55bed.mailgun.org>",
@@ -19,10 +35,49 @@ export default class App extends React.Component {
     });
   }
 
+  namiSearch(stufe, leiter){
+    this.nami.listMembers(stufe, leiter).then((data)=>{
+      console.log(data)
+      this.setState({namiData: data.map((elem)=>{
+        return elem.descriptor + "\t" + elem.entries_email;
+      }),
+    stufe: stufe,
+  leiter: leiter})
+    }, (error)=>{console.error(error)});
+  }
+
+  changeStufe(event, index, value) {
+    this.namiSearch(value, this.state.leiter)
+  }
+
   render() {
-    return (<div>
-      <h2>Welcome to React!</h2>
-      <a href="#" onClick={this.sendMail}>Klick mich</a>
-    </div>);
+    let namiData = null;
+    if(this.state.namiData){
+      namiData = this.state.namiData.map((elem, i)=>(<ListItem key={i}>{elem}</ListItem>))
+    }
+    let stufen = [];
+    for(let stufe in NamiAPI.Stufe){
+      stufen.push((<MenuItem primaryText={stufe} value={NamiAPI.Stufe[stufe]} key={NamiAPI.Stufe[stufe]}/>))
+    }
+    return (<MuiThemeProvider>
+
+
+      {(this.state.namiInitialized) ? (
+        <div>
+         <DropDownMenu value={this.state.stufe} onChange={this.changeStufe}>
+         {stufen}
+       </DropDownMenu>
+       <Toggle label="Leiter" toggled={this.state.leiter} onToggle={()=>{this.namiSearch(this.state.stufe, !this.state.leiter)}}/>
+       <Paper style={{maxHeight: "80vh", overflow: 'auto'}}>
+     <List>
+       {namiData}
+     </List>
+     </Paper>
+
+     </div>
+    ) : (<span>Nami lädt...</span>)}
+
+
+    </MuiThemeProvider>);
   }
 }
---
+++ src/lib/nami.js
@@ -0,0 +1,134 @@
+import request from 'request';
+
+
+
+
+export default class NamiAPI {
+  constructor(loginName, password){
+    this.loginName = loginName;
+    this.password = password;
+    this.host = "https://nami.dpsg.de";
+    this.authURL = "/ica/rest/nami/auth/manual/sessionStartup";
+    this.cookieJar = request.jar();
+    this.status = NamiAPI.Status.IDLE;
+    this.apiMajor = null;
+    this.apiMinor = null;
+    request.defaults({jar:true})
+  }
+
+  getSearchURL(){
+    return `${this.host}/ica/rest/api/${this.apiMajor}/${this.apiMinor}/service/nami/search/result-list`
+  }
+  /**
+   * Logs in the User.
+   * Returns Promise which is resolved once the user is logged in
+   */
+  startSession(){
+    return new Promise((resolve, reject)=>{
+
+      if(this.status == NamiAPI.Status.CONNECTED || this.status == NamiAPI.Status.AUTH){
+        console.warn("Auth Cancelled: Current Nami Status is CONNECTED or AUTH");
+        resolve(false);
+        return;
+      }
+      this.status = NamiAPI.Status.AUTH;
+      request.post({url: this.host + this.authURL,
+          jar: this.cookieJar,
+          form: {username:this.loginName, password: this.password, Login: "API", redirectTo: "./pages/loggedin.jsp"}
+        },  (error, response, body) => {
+        if(error){
+          this.status = NamiAPI.Status.ERROR;
+          reject(error)
+          return;
+        }
+        if (response.statusCode == 302) {
+            request.get({url: response.headers.location,
+              jar: this.cookieJar}, (error2, response2, body2) =>{
+                if(error2){
+                  this.status = NamiAPI.Status.ERROR;
+                  reject(error)
+                  return;
+                }
+                if(response2.statusCode != 200){
+                  this.status = NamiAPI.Status.ERROR;
+                  reject("AUTH Error: status code != 200 (in second response). Response:\n" + JSON.stringify(response2.toJSON()))
+                  return;
+                }
+                console.log("Successfully connected to Nami");
+                this.status = NamiAPI.Status.CONNECTED;
+                body2 = JSON.parse(body2);
+                this.apiMajor = body2.majorNumber;
+                this.apiMinor = body2.minorNumber;
+                resolve(true)
+              })
+        }else{
+          this.status = NamiAPI.Status.ERROR;
+          reject("AUTH Error: status code != 302. Response:\n" + JSON.stringify(response.toJSON()))
+        }
+      })
+
+    })
+
+  }
+/**
+ * Returns a promise to an array of members.
+ * @param {integer} stufe  which group we should search for.
+ * Use NamiAPI.Stufe for this
+ * @param {*} leiter (bool) if the group leaders should be included or not. Defaults to true
+ */
+  listMembers(stufe, leiter = true){
+    return this.search({
+      taetigkeitId: (leiter) ? 6 : " ",
+      mglStatusId: "AKTIV",
+      mglTypeId: "MITGLIED",
+      untergliederungId: stufe
+    })
+
+
+  }
+  /**
+   * Returns a promise to the full search
+   * @param {*} searchedValues what to search for. Possible keys (but not all of them); taetigkeitId, mglStatusId, mglTypeId, untergliederungId
+   */
+  search(searchedValues){
+    return new Promise((resolve, reject)=>{
+      if(this.status != NamiAPI.Status.CONNECTED){
+        reject("Authenticate before trying to search")
+      }
+      let params = {
+        searchedValues: JSON.stringify(searchedValues),
+        page: 1,
+        start: 0,
+        limit: 999999
+      }
+
+
+      request.get({url: this.getSearchURL(), qs: params, useQueryString: true, jar: this.cookieJar}, (error, response, body)=>{
+        resolve(JSON.parse(body).data)
+      })
+    })
+
+  }
+}
+
+
+
+NamiAPI.Status = Object.freeze({
+  //connection hasn't started
+  IDLE: 1,
+  //authentication has started
+  AUTH: 2,
+  // client is connected
+  CONNECTED: 3,
+  // an error occurred
+  ERROR: 99
+})
+
+//contains the untergliederungId for the search request
+NamiAPI.Stufe = Object.freeze({
+  WOE: 1,
+  JUPFI: 2,
+  PFADI: 3,
+  ROVER: 4,
+  STAVO: 5
+})
